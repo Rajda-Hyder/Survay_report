a4c08313bbf12d8dc3028af57e49d9d5bbf72627
Rajda-Hyder <rajdahyder@gmail.com>
2025-04-21 19:49:28 +0500

    Added name, roll no, and class details in index.html and convert_to_html.py

--- convert_to_html.py
+++ convert_to_html.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
+# -------------------------
+# Made by: Syeda Rajda Bano
+# Roll No: 00162420
+# Class: GIAIC
+# Day & Slot: Wednesday 7pm-10pm
+# -------------------------
+
 df = pd.read_csv("student_data.csv")
 df.to_html("index.html", index=False)
